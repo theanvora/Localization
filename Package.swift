9fbbef1ad9e7ea518911ea6d0a5c5b5800cdb32c
imx <phtuanh.imx@gmail.com>
2026-07-02 21:30:50 +0700

    chore: right-size minimum deployment target to the lowest API actually used

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Localization",
     platforms: [
-        .iOS("26.0")
+        .iOS(.v17)
     ],
     products: [
         .library(name: "Localization", targets: ["Localization"]),
